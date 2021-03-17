1fc8279924844e7db9094dd993fda085ac89dbfd
Gilleece <seangilleece@gmail.com>
2021-03-17 14:12:39 +0000

    Rebuilt register form using wtform

--- app.py
+++ app.py
@@ -5,11 +5,15 @@ from flask import (
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
+from forms import RegistrationForm
+from extensions import csrf
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
+
+csrf.init_app(app)
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
@@ -24,28 +28,42 @@ def list_materials():
     materials = mongo.db.materials.find()
     return render_template("materials.html", materials=materials)
 
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    if request.method == "POST":
+    name = None
+    email = None
+    password = None
+    form = RegistrationForm()
+    if form.validate_on_submit():        
+        name = form.name.data.lower()
+        email = form.email.data
+        password = generate_password_hash(form.password.data)
         existing_user = mongo.db.users.find_one(
-            {"username": request.form.get("username").lower()})
+            {"username": name})
 
         if existing_user:
             flash("Username already taken :(")
             return redirect(url_for("register"))
 
         register = {
-            "username": request.form.get("username").lower(),
-            "email": request.form.get("email"),
-            "password": generate_password_hash(request.form.get("password")),
+            "username": name,
+            "email": email,
+            "password": password,
             "admin": False
         }
-        mongo.db.users.insert_one(register)
+        mongo.db.users.insert_one(register)        
 
         # put the new user into session cookie
-        session["user"] = request.form.get("username").lower()
+        session["user"] = name
         flash("Thank you for signing up!")
-    return render_template("register.html")
+    return render_template(
+        "register.html",
+        form=form,
+        name=name,
+        email=email,
+        password=password
+    )
 
 
 if __name__ == "__main__":
@@ -53,3 +71,25 @@ if __name__ == "__main__":
             port=int(os.environ.get("PORT")),
             debug=True)
 # Debug must be false on submission!
+
+
+#    if request.method == "POST":
+#        existing_user = mongo.db.users.find_one(
+#            {"username": request.form.get("username").lower()})
+#
+#        if existing_user:
+#            flash("Username already taken :(")
+#            return redirect(url_for("register"))
+#
+#        register = {
+#            "username": request.form.get("username").lower(),
+#            "email": request.form.get("email"),
+#            "password": generate_password_hash(request.form.get("password")),
+#            "admin": False
+#        }
+#        mongo.db.users.insert_one(register)
+#
+#        # put the new user into session cookie
+#        session["user"] = request.form.get("username").lower()
+#        flash("Thank you for signing up!")
+#    return render_template("register.html")
---
+++ forms.py
@@ -0,0 +1,39 @@
+from flask_wtf import FlaskForm
+from wtforms import (
+    StringField,
+    TextAreaField,
+    SubmitField,
+    PasswordField,
+    DateField,
+    SelectField
+)
+from wtforms.validators import (
+    DataRequired,
+    Email,
+    EqualTo,
+    Length,
+    URL
+)
+
+
+
+class RegistrationForm(FlaskForm):
+    """Sign up for a user account."""
+    name = StringField(
+        'Username',
+        [DataRequired()]
+    )
+    email = StringField(
+        'Email',
+        [
+            Email(message='Not a valid email address.'),
+            DataRequired()
+        ]
+    )
+    password = PasswordField(
+        'Password',
+        [
+            DataRequired(message="Please enter a password."),
+        ]
+    )      
+    submit = SubmitField('Submit')
