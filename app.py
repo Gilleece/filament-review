e8bba05a25750393d58b57dc36be457c43dfd17a
Gilleece <seangilleece@gmail.com>
2021-03-19 22:09:49 +0000

    Add pla review page

--- app.py
+++ app.py
@@ -123,6 +123,12 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/pla")
+def pla():
+    reviews = mongo.db.reviews.find()
+    return render_template("materials/pla.html", reviews=reviews)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
