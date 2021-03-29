bd7836898490b7508febe25b321b3cc75197e0e3
Gilleece <seangilleece@gmail.com>
2021-03-29 21:24:05 +0000

    Add error handling for 404 and 500

--- app.py
+++ app.py
@@ -362,7 +362,17 @@ def wood():
     reviews = mongo.db.reviews.find()
     return render_template("materials/wood.html", reviews=reviews)
 # End of filament section app routes
-    
+
+
+# Error Handling
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
 
 
 if __name__ == "__main__":
